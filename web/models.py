2ffd3b7641dc491f3927dea9af88b2fe0fd63017
Zahra <zahra.abbc97@gmail.com>
2018-05-22 06:21:31 +0430

    Income model added (+some beatifications:D)

--- web/models.py
+++ web/models.py
@@ -11,4 +11,16 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{} - {}".format(self.date, self.amount)
+ 
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User) #yani harkodum az in income ha b y user vasl mishan
+    def __unicode__(self):
+        return "{} - {}".format(self.date, self.amount)
 
