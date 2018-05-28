813977e099a300a26214b35c78ba0b8078614ecd
ZahraAbbaC <zahra.abbc97@gmail.com>
2018-05-29 00:20:21 +0430

    add idea

--- web/models.py
+++ web/models.py
@@ -6,21 +6,31 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __unicode__(self):
+        return "{}_token".format(self.user)
+
+
 class Expense(models.Model):
     text = models.CharField (max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
     def __unicode__(self):
         return "{} - {}".format(self.date, self.amount)
  
-
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User) #yani harkodum az in income ha b y user vasl mishan
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #yani harkodum az in income ha b y user vasl mishan
+
     def __unicode__(self):
         return "{} - {}".format(self.date, self.amount)
 
--- web/views.py
+++ web/views.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from json import JSONEncoder
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from web.models import User, Income, Expense, Token
+from datetime import datetime
 from django.shortcuts import render
 
 # Create your views here.
+
+
+@csrf_exempt
+def submit_expense(request):
+    """user submits an expense"""
+
+#   TODO; validate data, user might be fake, token might be fake, amount might be...
+    if 'date' not in request.POST:
+        now = datetime.now()
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+    Expense.objects.create(user=this_user, amount=request.POST['amount'],
+                           text=request.POST['text'], date=now)
+    print ("i'm in submit expense")
+    print (request.POST)
+
+    return JsonResponse({
+        'status': 'ok',
+    }, encoder=JSONEncoder)
